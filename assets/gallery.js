979f77c6e08bb05b31a8d49286ea0562e977849d
xxoxx1309-byte <xxoxx1309@gmail.com>
2026-05-30 20:20:07 +0900

    Apply tweet text preview format

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,108 +1,122 @@
-﻿window.PORTFOLIO_ITEMS = [
+window.PORTFOLIO_ITEMS = [
   {
     "src": "https://pbs.twimg.com/media/FrCeZhwaYAAKh0n.jpg?name=orig",
-    "title": "AGF 2022",
-    "meta": "Eversoul / Dora / 2 photos",
+    "title": "[221203_AGF 2022]",
+    "meta": "에버소울 | 도라",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : 먕 님",
     "alt": "AGF 2022 에버소울 도라 코스프레 게시물",
     "url": "https://x.com/etett1309/status/1634980835488329728"
   },
   {
     "src": "https://pbs.twimg.com/media/FrCecUkacAUwMWb.jpg?name=orig",
-    "title": "AGF 2022",
-    "meta": "Eversoul / Dora / 1 photo",
-    "alt": "AGF 2022 에버소울 도라 코스프레 게시물",
+    "title": "[221203_AGF 2022]",
+    "meta": "에버소울 | 탈리아",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : @NEOUL_WAVE 님",
+    "alt": "AGF 2022 에버소울 탈리아 코스프레 게시물",
     "url": "https://x.com/etett1309/status/1634980952912068608"
   },
   {
     "src": "https://pbs.twimg.com/media/FrCfpnYaAAAYRoE.jpg?name=orig",
-    "title": "Archive 2023",
-    "meta": "Cosplay / 4 photos",
-    "alt": "2023 코스프레 사진 게시물",
+    "title": "[221218_12월 코스띠아모]",
+    "meta": "앙상블 스타즈 | 하루카와 소라",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : @bigabigabi2 님",
+    "alt": "12월 코스띠아모 앙상블 스타즈 하루카와 소라 코스프레 게시물",
     "url": "https://x.com/etett1309/status/1634982372969152513"
   },
   {
     "src": "https://pbs.twimg.com/media/FrCgOLnaUAA4Fii.jpg?name=orig",
-    "title": "Archive 2023",
-    "meta": "Cosplay / 4 photos",
-    "alt": "2023 코스프레 사진 게시물",
+    "title": "[221218_12월 코스띠아모]",
+    "meta": "앙상블 스타즈 | 하루카와 소라, 시노 하지메",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : @bigabigabi2 님, @Chaen_g 님",
+    "alt": "12월 코스띠아모 앙상블 스타즈 하루카와 소라 시노 하지메 코스프레 게시물",
     "url": "https://x.com/etett1309/status/1634982963493625859"
   },
   {
     "src": "https://pbs.twimg.com/media/FrCgykRaAAA2XrZ.jpg?name=orig",
-    "title": "Archive 2023",
-    "meta": "Cosplay / 2 photos",
-    "alt": "2023 코스프레 사진 게시물",
+    "title": "[221225_12월 서울 코믹월드]",
+    "meta": "앙상블 스타즈 | 사쿠마 레이",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : @NEKKEN_0409 님",
+    "alt": "12월 서울 코믹월드 앙상블 스타즈 사쿠마 레이 코스프레 게시물",
     "url": "https://x.com/etett1309/status/1634983691473813504"
   },
   {
     "src": "https://pbs.twimg.com/media/FrChYh8agAAXBGk.jpg?name=orig",
-    "title": "Archive 2023",
-    "meta": "Cosplay / 3 photos",
-    "alt": "2023 코스프레 사진 게시물",
+    "title": "[230304_3월 서울 코믹월드]",
+    "meta": "VOCALOID | 카가미네 린",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : @NOSUC_NOSUC 님",
+    "alt": "3월 서울 코믹월드 VOCALOID 카가미네 린 코스프레 게시물",
     "url": "https://x.com/etett1309/status/1634984654771191808"
   },
   {
     "src": "https://pbs.twimg.com/media/FrCh-4laMAAarzI.jpg?name=orig",
-    "title": "Archive 2023",
-    "meta": "Cosplay / 4 photos",
-    "alt": "2023 코스프레 사진 게시물",
+    "title": "[230305_3월 서울 코믹월드]",
+    "meta": "원신 | 벤티",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : @jeonjeon2140 님",
+    "alt": "3월 서울 코믹월드 원신 벤티 코스프레 게시물",
     "url": "https://x.com/etett1309/status/1634985080245592065"
   },
   {
     "src": "https://pbs.twimg.com/media/F209h3uasAALQOd.jpg?name=orig",
-    "title": "Archive 2023",
-    "meta": "Cosplay / 4 photos",
-    "alt": "2023 코스프레 사진 게시물",
+    "title": "[230720]",
+    "meta": "이터널 리턴 | 전학생 비앙카",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : @20M_RFT45_S2 님",
+    "alt": "이터널 리턴 전학생 비앙카 코스프레 게시물",
     "url": "https://x.com/etett1309/status/1688073232879087616"
   },
   {
     "src": "https://pbs.twimg.com/media/F20-DKZasAAhJTK.jpg?name=orig",
-    "title": "Archive 2023",
-    "meta": "Cosplay / 2 photos",
-    "alt": "2023 코스프레 사진 게시물",
+    "title": "[230731]",
+    "meta": "이터널 리턴 | 기본 제니 싱클레어",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : @164x167 님",
+    "alt": "이터널 리턴 기본 제니 싱클레어 코스프레 게시물",
     "url": "https://x.com/etett1309/status/1688073750552698880"
   },
   {
     "src": "https://pbs.twimg.com/media/F8CC-xnaYAAakxg.jpg?name=orig",
-    "title": "Archive 2023",
-    "meta": "Cosplay / 2 photos",
-    "alt": "2023 코스프레 사진 게시물",
+    "title": "[231007_10월 코믹월드]",
+    "meta": "프로젝트 세카이 | 요이사키 카나데",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : @nokcha87 님",
+    "alt": "10월 코믹월드 프로젝트 세카이 요이사키 카나데 코스프레 게시물",
     "url": "https://x.com/etett1309/status/1711511877451395167"
   },
   {
     "src": "https://pbs.twimg.com/media/GGrZStSa0AAt4Tc.jpg?name=orig",
-    "title": "Archive 2024",
-    "meta": "Cosplay / 4 photos",
-    "alt": "2024 코스프레 사진 게시물",
+    "title": "[240218_3회 일러스타페스]",
+    "meta": "슈퍼파리오 | 퀸부끄 & 쿠파히메",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : 나리땽님 (@nariddyang), 시루님 (@siru_0207)",
+    "alt": "3회 일러스타페스 슈퍼파리오 퀸부끄 쿠파히메 코스프레 게시물",
     "url": "https://x.com/etett1309/status/1759457547503972681"
   },
   {
     "src": "https://pbs.twimg.com/media/HJFWbijacAA0KoQ.jpg?name=orig",
-    "title": "Archive 2026",
-    "meta": "Cosplay / 3 photos",
-    "alt": "2026 코스프레 사진 게시물",
+    "title": "[260524_PlayX4]",
+    "meta": "이터널 리턴 | PlayX4",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : 천시아 (@sia_Caster)",
+    "alt": "PlayX4 이터널 리턴 코스프레 게시물",
     "url": "https://x.com/etett1309/status/2058521725898486027"
   },
   {
     "src": "https://pbs.twimg.com/media/HJI0i7maMAAp4kF.jpg?name=orig",
-    "title": "Archive 2026",
-    "meta": "Cosplay / 2 photos",
-    "alt": "2026 코스프레 사진 게시물",
+    "title": "[260524_PlayX4]",
+    "meta": "붕괴: 스타레일 | 키레네",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : 리리 (@riri_thisway)",
+    "alt": "PlayX4 붕괴 스타레일 키레네 코스프레 게시물",
     "url": "https://x.com/etett1309/status/2058765767890604205"
   },
   {
     "src": "https://pbs.twimg.com/media/HJI0nvHbsAAzEu6.jpg?name=orig",
-    "title": "Archive 2026",
-    "meta": "Cosplay / 2 photos",
-    "alt": "2026 코스프레 사진 게시물",
+    "title": "[260524_PlayX4]",
+    "meta": "붕괴: 스타레일 | 키레네",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : 리리 (@riri_thisway)",
+    "alt": "PlayX4 붕괴 스타레일 키레네 코스프레 게시물",
     "url": "https://x.com/etett1309/status/2058765845762072641"
   },
   {
     "src": "https://pbs.twimg.com/media/HJehzhraAAAAp1c.jpg?name=orig",
-    "title": "Archive 2026",
-    "meta": "Cosplay / 2 photos",
-    "alt": "2026 코스프레 사진 게시물",
+    "title": "[260524_PlayX4]",
+    "meta": "이터널 리턴 | 메이드 레니",
+    "model": "𝗠𝗢𝗗𝗘𝗟 : MIU (@miuu4u)",
+    "alt": "PlayX4 이터널 리턴 메이드 레니 코스프레 게시물",
     "url": "https://x.com/etett1309/status/2060293292290646350"
   }
 ];
-
