36ebe94d4c2076506dd57a2fd51b07ad966f0abb
xxoxx1309-byte <xxoxx1309@gmail.com>
2026-05-30 19:54:17 +0900

    Use empty gallery list

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,10 +1,1 @@
-window.PORTFOLIO_ITEMS = [
-  /*
-  {
-    src: "assets/gallery/sample-01.jpg",
-    title: "작업 제목",
-    meta: "Cosplay / Seoul / 2026",
-    alt: "사진 설명"
-  }
-  */
-];
+window.PORTFOLIO_ITEMS = [];
