961601f14adacf4ab13599a9fc6742b1a9d1d140
xxoxx1309-byte <xxoxx1309@gmail.com>
2026-05-30 20:13:21 +0900

    Use first image previews for post cards

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,6 +1,6 @@
-window.PORTFOLIO_ITEMS = [
+﻿window.PORTFOLIO_ITEMS = [
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/1634980835488329728/FrCeZhwaYAAKh0n/FrCeZiHacAIOGBq",
+    "src": "https://pbs.twimg.com/media/FrCeZhwaYAAKh0n.jpg?name=orig",
     "title": "AGF 2022",
     "meta": "Eversoul / Dora / 2 photos",
     "alt": "AGF 2022 에버소울 도라 코스프레 게시물",
@@ -14,94 +14,95 @@ window.PORTFOLIO_ITEMS = [
     "url": "https://x.com/etett1309/status/1634980952912068608"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/1634982372969152513/FrCfpnYaAAAYRoE/FrCfpnYakAAmE-_/FrCfpnbaMAAETjk/FrCfpnaacAACBF_",
+    "src": "https://pbs.twimg.com/media/FrCfpnYaAAAYRoE.jpg?name=orig",
     "title": "Archive 2023",
     "meta": "Cosplay / 4 photos",
     "alt": "2023 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/1634982372969152513"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/1634982963493625859/FrCgOLnaUAA4Fii/FrCgOL7aIAIZeH-/FrCgOMRagAAGIxE/FrCgOMlaMAEyQHi",
+    "src": "https://pbs.twimg.com/media/FrCgOLnaUAA4Fii.jpg?name=orig",
     "title": "Archive 2023",
     "meta": "Cosplay / 4 photos",
     "alt": "2023 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/1634982963493625859"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/1634983691473813504/FrCgykRaAAA2XrZ/FrCgykeacAAYJJK",
+    "src": "https://pbs.twimg.com/media/FrCgykRaAAA2XrZ.jpg?name=orig",
     "title": "Archive 2023",
     "meta": "Cosplay / 2 photos",
     "alt": "2023 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/1634983691473813504"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/1634984654771191808/FrChYh8agAAXBGk/FrChYiLaYAASm3U/FrChYiWagAEPT9-",
+    "src": "https://pbs.twimg.com/media/FrChYh8agAAXBGk.jpg?name=orig",
     "title": "Archive 2023",
     "meta": "Cosplay / 3 photos",
     "alt": "2023 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/1634984654771191808"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/1634985080245592065/FrCh-4laMAAarzI/FrCh-45acAAz0tz/FrCh-5LaIAg_oCx/FrCh-5makAEjz73",
+    "src": "https://pbs.twimg.com/media/FrCh-4laMAAarzI.jpg?name=orig",
     "title": "Archive 2023",
     "meta": "Cosplay / 4 photos",
     "alt": "2023 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/1634985080245592065"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/1688073232879087616/F209h3uasAALQOd/F209h3tbIAAv5Bs/F209h31a0AAIaFc/F209h3vbYAADG-r",
+    "src": "https://pbs.twimg.com/media/F209h3uasAALQOd.jpg?name=orig",
     "title": "Archive 2023",
     "meta": "Cosplay / 4 photos",
     "alt": "2023 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/1688073232879087616"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/1688073750552698880/F20-DKZasAAhJTK/F20-DKWawAAnBlD",
+    "src": "https://pbs.twimg.com/media/F20-DKZasAAhJTK.jpg?name=orig",
     "title": "Archive 2023",
     "meta": "Cosplay / 2 photos",
     "alt": "2023 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/1688073750552698880"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/1711511877451395167/F8CC-xnaYAAakxg/F8CC-yIasAAs51d",
+    "src": "https://pbs.twimg.com/media/F8CC-xnaYAAakxg.jpg?name=orig",
     "title": "Archive 2023",
     "meta": "Cosplay / 2 photos",
     "alt": "2023 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/1711511877451395167"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/1759457547503972681/GGrZStSa0AAt4Tc/GGrZStuaIAEJEY6/GGrZTRcaMAAZWBl/GGrZTrXakAAo7bW",
+    "src": "https://pbs.twimg.com/media/GGrZStSa0AAt4Tc.jpg?name=orig",
     "title": "Archive 2024",
     "meta": "Cosplay / 4 photos",
     "alt": "2024 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/1759457547503972681"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/2058521725898486027/HJFWbijacAA0KoQ/HJFWj_VaUAAzyK_/HJFWkBuaoAA4_3A",
+    "src": "https://pbs.twimg.com/media/HJFWbijacAA0KoQ.jpg?name=orig",
     "title": "Archive 2026",
     "meta": "Cosplay / 3 photos",
     "alt": "2026 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/2058521725898486027"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/2058765767890604205/HJI0i7maMAAp4kF/HJI0i6saEAAZe6k",
+    "src": "https://pbs.twimg.com/media/HJI0i7maMAAp4kF.jpg?name=orig",
     "title": "Archive 2026",
     "meta": "Cosplay / 2 photos",
     "alt": "2026 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/2058765767890604205"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/2058765845762072641/HJI0nvHbsAAzEu6/HJI0nvXbsAEijXB",
+    "src": "https://pbs.twimg.com/media/HJI0nvHbsAAzEu6.jpg?name=orig",
     "title": "Archive 2026",
     "meta": "Cosplay / 2 photos",
     "alt": "2026 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/2058765845762072641"
   },
   {
-    "src": "https://mosaic.fxtwitter.com/jpeg/2060293292290646350/HJehzhraAAAAp1c/HJehzhqbAAAmlku",
+    "src": "https://pbs.twimg.com/media/HJehzhraAAAAp1c.jpg?name=orig",
     "title": "Archive 2026",
     "meta": "Cosplay / 2 photos",
     "alt": "2026 코스프레 사진 게시물",
     "url": "https://x.com/etett1309/status/2060293292290646350"
   }
 ];
+
