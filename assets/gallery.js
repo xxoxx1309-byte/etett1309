5420dcb5e0e481900109f8cadcb737a21f78eb39
xxoxx1309-byte <xxoxx1309@gmail.com>
2026-05-30 20:29:10 +0900

    Add face-focused preview positions

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,11 +1,12 @@
-window.PORTFOLIO_ITEMS = [
+﻿window.PORTFOLIO_ITEMS = [
   {
     "src": "https://pbs.twimg.com/media/FrCeZhwaYAAKh0n.jpg?name=orig",
     "title": "[221203_AGF 2022]",
     "meta": "에버소울 | 도라",
     "model": "𝗠𝗢𝗗𝗘𝗟 : 먕 님",
     "alt": "AGF 2022 에버소울 도라 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/1634980835488329728"
+    "url": "https://x.com/etett1309/status/1634980835488329728",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/FrCecUkacAUwMWb.jpg?name=orig",
@@ -13,7 +14,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "에버소울 | 탈리아",
     "model": "𝗠𝗢𝗗𝗘𝗟 : @NEOUL_WAVE 님",
     "alt": "AGF 2022 에버소울 탈리아 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/1634980952912068608"
+    "url": "https://x.com/etett1309/status/1634980952912068608",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/FrCfpnYaAAAYRoE.jpg?name=orig",
@@ -21,7 +23,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "앙상블 스타즈 | 하루카와 소라",
     "model": "𝗠𝗢𝗗𝗘𝗟 : @bigabigabi2 님",
     "alt": "12월 코스띠아모 앙상블 스타즈 하루카와 소라 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/1634982372969152513"
+    "url": "https://x.com/etett1309/status/1634982372969152513",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/FrCgOLnaUAA4Fii.jpg?name=orig",
@@ -29,7 +32,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "앙상블 스타즈 | 하루카와 소라, 시노 하지메",
     "model": "𝗠𝗢𝗗𝗘𝗟 : @bigabigabi2 님, @Chaen_g 님",
     "alt": "12월 코스띠아모 앙상블 스타즈 하루카와 소라 시노 하지메 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/1634982963493625859"
+    "url": "https://x.com/etett1309/status/1634982963493625859",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/FrCgykRaAAA2XrZ.jpg?name=orig",
@@ -37,7 +41,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "앙상블 스타즈 | 사쿠마 레이",
     "model": "𝗠𝗢𝗗𝗘𝗟 : @NEKKEN_0409 님",
     "alt": "12월 서울 코믹월드 앙상블 스타즈 사쿠마 레이 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/1634983691473813504"
+    "url": "https://x.com/etett1309/status/1634983691473813504",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/FrChYh8agAAXBGk.jpg?name=orig",
@@ -45,7 +50,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "VOCALOID | 카가미네 린",
     "model": "𝗠𝗢𝗗𝗘𝗟 : @NOSUC_NOSUC 님",
     "alt": "3월 서울 코믹월드 VOCALOID 카가미네 린 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/1634984654771191808"
+    "url": "https://x.com/etett1309/status/1634984654771191808",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/FrCh-4laMAAarzI.jpg?name=orig",
@@ -53,7 +59,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "원신 | 벤티",
     "model": "𝗠𝗢𝗗𝗘𝗟 : @jeonjeon2140 님",
     "alt": "3월 서울 코믹월드 원신 벤티 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/1634985080245592065"
+    "url": "https://x.com/etett1309/status/1634985080245592065",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/F209h3uasAALQOd.jpg?name=orig",
@@ -61,7 +68,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "이터널 리턴 | 전학생 비앙카",
     "model": "𝗠𝗢𝗗𝗘𝗟 : @20M_RFT45_S2 님",
     "alt": "이터널 리턴 전학생 비앙카 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/1688073232879087616"
+    "url": "https://x.com/etett1309/status/1688073232879087616",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/F20-DKZasAAhJTK.jpg?name=orig",
@@ -69,7 +77,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "이터널 리턴 | 기본 제니 싱클레어",
     "model": "𝗠𝗢𝗗𝗘𝗟 : @164x167 님",
     "alt": "이터널 리턴 기본 제니 싱클레어 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/1688073750552698880"
+    "url": "https://x.com/etett1309/status/1688073750552698880",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/F8CC-xnaYAAakxg.jpg?name=orig",
@@ -77,7 +86,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "프로젝트 세카이 | 요이사키 카나데",
     "model": "𝗠𝗢𝗗𝗘𝗟 : @nokcha87 님",
     "alt": "10월 코믹월드 프로젝트 세카이 요이사키 카나데 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/1711511877451395167"
+    "url": "https://x.com/etett1309/status/1711511877451395167",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/GGrZStSa0AAt4Tc.jpg?name=orig",
@@ -85,7 +95,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "슈퍼파리오 | 퀸부끄 & 쿠파히메",
     "model": "𝗠𝗢𝗗𝗘𝗟 : 나리땽님 (@nariddyang), 시루님 (@siru_0207)",
     "alt": "3회 일러스타페스 슈퍼파리오 퀸부끄 쿠파히메 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/1759457547503972681"
+    "url": "https://x.com/etett1309/status/1759457547503972681",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/HJFWbijacAA0KoQ.jpg?name=orig",
@@ -93,7 +104,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "이터널 리턴 | PlayX4",
     "model": "𝗠𝗢𝗗𝗘𝗟 : 천시아 (@sia_Caster)",
     "alt": "PlayX4 이터널 리턴 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/2058521725898486027"
+    "url": "https://x.com/etett1309/status/2058521725898486027",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/HJI0i7maMAAp4kF.jpg?name=orig",
@@ -101,7 +113,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "붕괴: 스타레일 | 키레네",
     "model": "𝗠𝗢𝗗𝗘𝗟 : 리리 (@riri_thisway)",
     "alt": "PlayX4 붕괴 스타레일 키레네 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/2058765767890604205"
+    "url": "https://x.com/etett1309/status/2058765767890604205",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/HJI0nvHbsAAzEu6.jpg?name=orig",
@@ -109,7 +122,8 @@ window.PORTFOLIO_ITEMS = [
     "meta": "붕괴: 스타레일 | 키레네",
     "model": "𝗠𝗢𝗗𝗘𝗟 : 리리 (@riri_thisway)",
     "alt": "PlayX4 붕괴 스타레일 키레네 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/2058765845762072641"
+    "url": "https://x.com/etett1309/status/2058765845762072641",
+    "focus": "50% 24%"
   },
   {
     "src": "https://pbs.twimg.com/media/HJehzhraAAAAp1c.jpg?name=orig",
@@ -117,12 +131,9 @@ window.PORTFOLIO_ITEMS = [
     "meta": "이터널 리턴 | 메이드 레니",
     "model": "𝗠𝗢𝗗𝗘𝗟 : MIU (@miuu4u)",
     "alt": "PlayX4 이터널 리턴 메이드 레니 코스프레 게시물",
-    "url": "https://x.com/etett1309/status/2060293292290646350"
-  },
-  {
-    "src": "assets/gallery/dd.jpg",
-    "title": "효광",
-    "meta": "11",
-    "alt": "111"
+    "url": "https://x.com/etett1309/status/2060293292290646350",
+    "focus": "50% 24%"
   }
 ];
+
+
